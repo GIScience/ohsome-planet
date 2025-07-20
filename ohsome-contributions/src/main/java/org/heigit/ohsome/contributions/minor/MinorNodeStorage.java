f102683f0271d784e310426f6ef66c8570ecbc1e
Rafael Troilo <rafael.troilo@heigit.org>
2025-07-20 14:47:36 +0200

    fix: sonar issues

--- ohsome-contributions/src/main/java/org/heigit/ohsome/contributions/minor/MinorNodeStorage.java
+++ ohsome-contributions/src/main/java/org/heigit/ohsome/contributions/minor/MinorNodeStorage.java
@@ -13,8 +13,8 @@ import java.util.Set;
 public interface MinorNodeStorage extends AutoCloseable {
 
     static MinorNodeStorage inRocksMap(Path path) throws RocksDBException, IOException {
+        final var db = new RocksMap(path);
         return new MinorNodeStorage() {
-            final RocksMap db = new RocksMap(path);
             @Override
             public Map<Long, List<OSMNode>> getNodes(Set<Long> nodes) {
                 return db.get(nodes, MinorNode::deserialize);
