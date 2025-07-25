9f52f93c1dd4b4a2dd454251faa58a0874c074a7
Rafael Troilo <rafael.troilo@heigit.org>
2025-07-25 17:51:08 +0200

    fix: GenericType for RawMessageEncoder

--- ohsome-contributions/src/test/java/org/heigit/ohsome/avro/OSMAvroTest.java
+++ ohsome-contributions/src/test/java/org/heigit/ohsome/avro/OSMAvroTest.java
@@ -11,18 +11,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class OSMAvroTest {
 
     @Test
     void encodeDecode() throws IOException {
         var model = new SpecificData();
-        var encoder = new RawMessageEncoder<>(model, OSMNode.getClassSchema());
-        var decoder = new RawMessageDecoder<>(model, OSMNode.getClassSchema());
+        var encoder = new RawMessageEncoder<OSMNode>(model, OSMNode.getClassSchema());
+        var decoder = new RawMessageDecoder<OSMNode>(model, OSMNode.getClassSchema());
         var data = encoder.encode(OSMNode.newBuilder()
                 .setTimestamp(Instant.now().getEpochSecond())
                 .setTags(Map.of("natrual","tree"))
                 // 49.4184793, 8.6756824
-                .setLon(86756824).setLat(494184793)
+                .setLon(8_6756824).setLat(49_4184793)
                 .setBackRefsWay(List.of(
                         123L, 234L
                 ))
@@ -32,6 +34,7 @@ class OSMAvroTest {
         System.out.printf("data[%d] = %s%n",data.length, Arrays.toString(data));
         var avroNode = decoder.decode(data);
         System.out.println("avroNode = " + avroNode);
+        assertEquals(8_6756824L, avroNode.getLon());
     }
 
 }
