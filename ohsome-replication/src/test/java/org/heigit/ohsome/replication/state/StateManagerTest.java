1447617ae0132daaf728fa83a24ceaea37c75c6e
jstier <stier.jochen@web.de>
2025-08-06 11:50:03 +0200

    feat: add parsing of osm sources to StateManager

--- ohsome-replication/src/test/java/org/heigit/ohsome/replication/state/StateManagerTest.java
+++ ohsome-replication/src/test/java/org/heigit/ohsome/replication/state/StateManagerTest.java
@@ -15,4 +15,31 @@ public class StateManagerTest {
         var contributionState = contributionStateManager.getRemoteState();
         System.out.println("contributionState = " + contributionState);
     }
+
+    @Test
+    public void testStateManagerGetRemoteReplicationData() {
+        var changesetStateManager = new ChangesetStateManager();
+        var contributionStateManager = new ContributionStateManager("minute");
+
+        var changesetReplicationFile = changesetStateManager.getReplicationFile("000/021/212");
+        System.out.println("changesetReplicationFile = " + changesetReplicationFile);
+        var contributionReplicationFile = contributionStateManager.getReplicationFile("000/021/212");
+        System.out.println("contributionReplicationFile = " + contributionReplicationFile);
+    }
+
+
+    @Test
+    public void testFetchReplicationBatch() {
+        var changesetStateManager = new ChangesetStateManager();
+        var batch = changesetStateManager.fetchReplicationBatch("000/021/212");
+        for (var batchElement : batch) {
+            System.out.println(batchElement);
+        }
+
+        var contributionStateManager = new ContributionStateManager("minute");
+        var contributionBatch = contributionStateManager.fetchReplicationBatch("000/021/212");
+        for (var batchElement : contributionBatch) {
+            System.out.println(batchElement);
+        }
+    }
 }
