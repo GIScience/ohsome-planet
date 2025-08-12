e73eab78884050029f5de729f88021d72ecc2612
jstier <stier.jochen@web.de>
2025-08-12 12:19:27 +0200

    feat: add changeset replication functionality

--- ohsome-replication/src/test/java/org/heigit/ohsome/replication/state/ReplicationStateTest.java
+++ ohsome-replication/src/test/java/org/heigit/ohsome/replication/state/ReplicationStateTest.java
@@ -11,7 +11,7 @@ public class ReplicationStateTest {
         var replicationState = new ReplicationState(now(), 5325622);
         var replicationState2 = new ReplicationState(now(), 1);
 
-        assertEquals("005/325/622", replicationState.sequenceNumberAsPath());
-        assertEquals("000/000/001", replicationState2.sequenceNumberAsPath());
+        assertEquals("005/325/622", ReplicationState.sequenceNumberAsPath(replicationState.sequenceNumber));
+        assertEquals("000/000/001", ReplicationState.sequenceNumberAsPath(replicationState2.sequenceNumber));
     }
 }
