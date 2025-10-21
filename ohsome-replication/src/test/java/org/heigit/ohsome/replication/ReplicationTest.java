c27df71c8505a0a6cbeb5e86442ff41aaef71746
Rafael Troilo <rafael.troilo@heigit.org>
2025-10-21 16:19:34 +0200

    various things

--- ohsome-replication/src/test/java/org/heigit/ohsome/replication/ReplicationTest.java
+++ ohsome-replication/src/test/java/org/heigit/ohsome/replication/ReplicationTest.java
@@ -1,19 +1,51 @@
 package org.heigit.ohsome.replication;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
-import java.io.IOException;
 import java.nio.file.Path;
 
 /**
  * Unit test for simple App.
  */
 class ReplicationTest {
-    private static final String RESOURCE_PATH = "./src/test/resources";
+    private static final Path RESOURCE_PATH = Path.of("src/test/resources/");
+
+    @Container
+    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgis/postgis:17-3.5")
+                    .asCompatibleSubstituteFor("postgres"))
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+    private static String dbUrl;
+
+    @BeforeAll
+    static void setUp() {
+        postgresContainer.withInitScript("setupDB/setupReplicationUpdate.sql");
+        postgresContainer.start();
+        dbUrl = postgresContainer.getJdbcUrl() + "&user=" + postgresContainer.getUsername() + "&password=" + postgresContainer.getPassword();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        postgresContainer.stop();
+    }
 
 
     @Test
-    void testReplication() throws IOException {
-          ReplicationManager.update("minute", Path.of(RESOURCE_PATH), System.getProperty("DB_URL"));
+    void testReplication() throws Exception {
+        var replicationChangesetUrl = RESOURCE_PATH.resolve("replication/changesets").toUri().toURL().toString();
+        var ohsomePlanetPath = RESOURCE_PATH.resolve("ohsome-planet");
+        System.out.println("replicationChangesetUrl = " + replicationChangesetUrl);
+
+//        var changesetManager = new ChangesetStateManager(replicationChangesetUrl, dbUrl);
+//        changesetManager.initializeLocalState();
+
+        ReplicationManager.update(ohsomePlanetPath, dbUrl, replicationChangesetUrl);
     }
 }
