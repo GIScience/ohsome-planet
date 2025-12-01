0d539375741e3203cf3899de0927f3c55b4111b6
jstier <stier.jochen@web.de>
2025-12-01 16:45:55 +0100

    chore: remove unused ReplicationServer

--- ohsome-replication/src/test/java/org/heigit/ohsome/replication/ServerTest.java
+++ ohsome-replication/src/test/java/org/heigit/ohsome/replication/ServerTest.java
@@ -4,15 +4,11 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import static org.heigit.ohsome.replication.ReplicationServer.tryToFindStartFromTimestamp;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ServerTest {
@@ -59,28 +55,6 @@ class ServerTest {
             assertTrue(maxChangesetDbTimestamp.isAfter(oldReplication.getTimestamp()));
             var secondsBetween = Duration.between(maxChangesetDbTimestamp, oldReplication.getTimestamp()).toSeconds();
             assertTrue(secondsBetween < 120);
-        }
-    }
-
-    @Test
-    @Disabled
-    void testGettingOldSequenceNumberFromOldTimestampWithOtherMethod() throws IOException, URISyntaxException {
-        var formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSSSSS XXX");
-        var instant = OffsetDateTime.parse("2025-08-14 11:51:33.163329000 +00:00", formatter).toInstant();
-
-        for (var time : List.of(
-                "2025-08-02T00:00:00Z",
-                "2025-08-04T00:10:12Z")) {
-            var maxChangesetDbTimestamp = Instant.parse(time);
-            var oldReplication = tryToFindStartFromTimestamp(
-                    new URI("https://planet.openstreetmap.org/replication/minute").toURL(),
-                    maxChangesetDbTimestamp
-            );
-            System.out.println("oldReplication = " + oldReplication);
-            System.out.println("timestamp = " + time);
-            assertTrue(maxChangesetDbTimestamp.isBefore((Instant) oldReplication.get("timestamp")));
-            //var secondsBetween = Duration.between(maxChangesetDbTimestamp, oldReplication.getTimestamp()).toSeconds();
-            //assertTrue(secondsBetween < 80);
         }
     }
 }
