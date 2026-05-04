9dbd44322598c833f71aed35dc9bfd8410841938
Rafael Troilo <rafael.troilo@heigit.org>
2026-05-04 16:29:49 +0200

    feat: add correct geoparquet crs EPGS4226 specification

--- ohsome-parquet/src/main/java/org/heigit/ohsome/parquet/GeoParquet.java
+++ ohsome-parquet/src/main/java/org/heigit/ohsome/parquet/GeoParquet.java
@@ -10,6 +10,40 @@ import static java.util.stream.Collectors.joining;
 
 public class GeoParquet<T> {
 
+    private static final String CRS_EPSG_4326 = """
+             {
+                     "$schema": "https://proj.org/schemas/v0.7/projjson.schema.json",
+                     "type": "GeographicCRS",
+                     "name": "WGS 84",
+                     "datum_ensemble": {
+                       "name": "World Geodetic System 1984 ensemble",
+                       "members": [
+                         { "name": "World Geodetic System 1984 (Transit)", "id": { "authority": "EPSG", "code": 1166 }},
+                         { "name": "World Geodetic System 1984 (G730)",    "id": { "authority": "EPSG", "code": 1152 }},
+                         { "name": "World Geodetic System 1984 (G873)",    "id": { "authority": "EPSG", "code": 1153 }},
+                         { "name": "World Geodetic System 1984 (G1150)",   "id": { "authority": "EPSG", "code": 1154 }},
+                         { "name": "World Geodetic System 1984 (G1674)",   "id": { "authority": "EPSG", "code": 1155 }},
+                         { "name": "World Geodetic System 1984 (G1762)",   "id": { "authority": "EPSG", "code": 1156 }},
+                         { "name": "World Geodetic System 1984 (G2139)",   "id": { "authority": "EPSG", "code": 1309 }},
+                         { "name": "World Geodetic System 1984 (G2296)",   "id": { "authority": "EPSG", "code": 1383 }},
+                       ],
+                       "ellipsoid": { "name": "WGS 84", "semi_major_axis": 6378137, "inverse_flattening": 298.257223563 },
+                       "accuracy": "2.0",
+                       "id": {"authority": "EPSG", "code": 6326}
+                     },
+                     "coordinate_system": {
+                       "subtype": "ellipsoidal",
+                       "axis": [
+                         { "name": "Geodetic latitude", "abbreviation": "Lat", "direction": "north", "unit": "degree" },
+                         { "name": "Geodetic longitude", "abbreviation": "Lon", "direction": "east", "unit": "degree" },
+                       ]
+                     },
+                     "scope": "Horizontal component of 3D system.",
+                     "area": "World.",
+                     "bbox": { "south_latitude": -90, "west_longitude": -180, "north_latitude": 90, "east_longitude": 180 },
+                     "id": {"authority": "EPSG", "code": 4326},
+                   }""";
+
     public static class GeoParquetBuilder<T> {
         private final List<Column<T>> columns = new ArrayList<>();
 
@@ -76,9 +110,16 @@ public class GeoParquet<T> {
         }
 
         public String print(Envelope bbox) {
-            return "\"%s\": { \"encoding\": \"%s\", \"bbox\": %s%s, \"geometry_types\": %s }".formatted(
+            return """
+                    "%s": {
+                          "encoding": "%s",
+                          "crs": %s,
+                          "bbox": %s%s,
+                          "geometry_types": %s
+                        }""".formatted(
                     name,
                     encoding.schema(),
+                    CRS_EPSG_4326,
                     "[%s, %s, %s, %s]".formatted(bbox.getMinX(), bbox.getMinY(), bbox.getMaxX(), bbox.getMaxY()),
                     covering == null ? "" : ", \"covering\": { %s }".formatted(coveringString()),
                     geometryTypeString());
